71d3f2ed2642ea2833380478efb5fce4a087c109
amitjamwal <bikujamwal@gmail.com>
2025-07-11 14:50:48 +0530

    export app file changes

--- server.js
+++ server.js
@@ -14,10 +14,4 @@ app.use(express.json());
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
-const PORT = process.env.PORT || 5000;
-
-// Remove or comment out the following line:
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// Add this line at the end:
 module.exports = app;
