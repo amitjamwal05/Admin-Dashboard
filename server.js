ae71b55fa253c7e094e0f4e56154cfea30a51385
amitjamwal <bikujamwal@gmail.com>
2025-07-11 14:56:16 +0530

    API UPDATE

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Add this route for root path
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
